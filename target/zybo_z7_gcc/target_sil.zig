8d1f12b84675cfdb3d98790201a80ee7dbb6563b
Hiroaki TAKADA <hiro@ertl.jp>
2020-08-11 15:39:01 +0900

    sil_dly のアラインメント単位を8バイトに変更。

--- target/zybo_z7_gcc/target_sil.zig
+++ target/zybo_z7_gcc/target_sil.zig
@@ -16,10 +16,11 @@ pub usingnamespace @import("../../arch/arm_gcc/common/core_sil.zig");
 ///
 ///  微少時間待ち
 ///
-///  キャッシュラインのどの場所にあるかのよって実行時間が変わるため，
-///  大きめの単位でアラインしている．インライン展開されると，アライン
-///  指定が無効になるため，インライン展開しないようにしている．
+///  ダブルワード（64ビット）のどちらに命令があるかのよって実行時間が
+///  変わる可能性があると考え，64ビット境界にアラインしている．インラ
+///  イン展開されると，アライン指定が無効になるため，インライン展開し
+///  ないようにしている．
 ///
-pub noinline fn dly_nse(dlytim: usize) align(256) void {
+pub noinline fn dly_nse(dlytim: usize) align(8) void {
     core_dly_nse(dlytim, DLY_TIM1, DLY_TIM2);
 }
